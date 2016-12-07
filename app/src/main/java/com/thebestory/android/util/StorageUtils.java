2f8e76b0b08256a261288fb1728d6963ba19f348
Nariman Safiulin <woofilee@gmail.com>
2016-12-07 12:41:00 +0300

    Utils updated

--- app/src/main/java/com/thebestory/android/util/StorageUtils.java
+++ app/src/main/java/com/thebestory/android/util/StorageUtils.java
@@ -12,54 +12,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 /**
- * Class provides a methods for reading and writing files on the File System.
+ * Class provides a methods for file system management.
  * TODO: Implement methods for working w/ external Android storage
  */
 public final class StorageUtils {
     enum StorageType {
         INTERNAL,
         EXTERNAL
-    }
-
-    /**
-     * Returns {@link FileInputStream} to read, and, if needed, creates full path to the file.
-     *
-     * @param filepath full path to the file
-     * @param context  application context
-     * @return {@link FileInputStream} instance to read
-     * @throws FileNotFoundException if path to the file not created, and file cannot be available
-     */
-    public static FileInputStream getInputFile(String filepath,
-                                               Context context) throws FileNotFoundException {
-        return new FileInputStream(get(filepath, context));
-    }
-
-    /**
-     * Returns {@link FileOutputStream} to write, and, if needed, creates full path to the file.
-     *
-     * @param filepath full path to the file
-     * @param context  application context
-     * @return {@link FileOutputStream} instance to write
-     * @throws FileNotFoundException if path to the file not created, and file cannot be available
-     */
-    public static FileOutputStream getOutputFile(String filepath,
-                                                 Context context) throws FileNotFoundException {
-        return new FileOutputStream(get(filepath, context));
-    }
-
-    /**
-     * Deletes file or folder by abstract path. If this path points to folder, then this folder must
-     * be empty.
-     *
-     * @param path    full path
-     * @param context application context
-     * @return {@code true}, if file or folder is deleted (or not exists), otherwise {@code false}
-     * @throws FileNotFoundException
-     */
-    public static boolean delete(String path,
-                                 Context context) throws FileNotFoundException {
-        File file = new File(context.getFilesDir(), path);
-        return !file.exists() || file.delete();
     }
 
     /**
@@ -82,6 +41,21 @@ public final class StorageUtils {
         }
 
         return current;
+    }
+
+    /**
+     * Deletes file or folder by abstract path. If this path points to folder, then this folder must
+     * be empty.
+     *
+     * @param path    full path
+     * @param context application context
+     * @return {@code true}, if file or folder is deleted (or not exists), otherwise {@code false}
+     * @throws FileNotFoundException
+     */
+    public static boolean delete(String path,
+                                 Context context) throws FileNotFoundException {
+        File file = new File(context.getFilesDir(), path);
+        return !file.exists() || file.delete();
     }
 
     private StorageUtils() {
